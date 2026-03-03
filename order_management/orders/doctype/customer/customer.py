662900e4fece8db24465dfd9f9e46aaa5d768e6b
shrikant <shrikant.b@caratred.com>
2026-03-03 08:37:52 +0530

    create a customer using insert

--- order_management/orders/doctype/customer/customer.py
+++ order_management/orders/doctype/customer/customer.py
@@ -6,3 +6,23 @@ from frappe.model.document import Document
 
 class Customer(Document):
 	pass
+
+import frappe
+
+
+@frappe.whitelist()
+def create_customer():
+    doc = frappe.get_doc({
+        "doctype": "Customer",
+    "customer_name": "Test Customer",
+    "email": "test@gmail.com",
+    "phone": "9876543210",
+    "status": "Active"
+    })
+    doc.insert(ignore_permissions=True)
+    return {"message": f"{doc.customer_name} has been created successfully"}
+
+
+
+
+
